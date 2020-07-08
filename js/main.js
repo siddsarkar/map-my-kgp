315ea05ff570e263f51c2ccafdfe9248c0a1e994
Siddhartha Sarkar <siddsarkar7@gmail.com>
2020-07-08 17:08:50 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const matchList = document.getElementById('match-list');
 
 //search rooms.json and filter it
 const searchClass = async searchText => {
-    const res = await fetch('http://siddsarkar.me/data/');
+    const res = await fetch('http://siddsarkar.me/data/index.json');
     const rooms = await res.json();
 
     //get match to input
