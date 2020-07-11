9e3a84505cca4896157c8b3ac8d131372a2ef0de
unknown <siddsarkar7@gmail.com>
2020-07-11 19:58:23 +0530

    icons added

--- js/themeswitcher.js
+++ js/themeswitcher.js
@@ -1,6 +1,7 @@
 let current = document.getElementById('theme');
 let swtchrbtn = document.getElementById('themetoggle');
 let toggleIcon = document.getElementById('themetoggle-icon');
+let appIcon = document.getElementById('app-icon');
 
 function switchTheme() {
     if (current.getAttribute('href') == 'res/bootswatch/[cosmo]bootstrap.min.css') {
@@ -8,6 +9,7 @@ function switchTheme() {
         current.href = 'res/bootswatch/[cyborg]bootstrap.min.css';
         swtchrbtn.style.backgroundColor = 'white';
         toggleIcon.style.color = '#1b1b1b';
+        appIcon.src = 'icons/app_icon_light.png'
 
     }
     else {
@@ -15,5 +17,6 @@ function switchTheme() {
         current.href = 'res/bootswatch/[cosmo]bootstrap.min.css';
         swtchrbtn.style.backgroundColor = '#1b1b1b';
         toggleIcon.style.color = 'white';
+        appIcon.src = 'icons/app_icon_dark.png'
     }
 }
