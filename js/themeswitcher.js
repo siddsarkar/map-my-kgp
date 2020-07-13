dc81248f9fbcb7d6e1a573cd6dff36e950ca257c
unknown <siddsarkar7@gmail.com>
2020-07-13 22:30:29 +0530

    icon and theme

--- js/themeswitcher.js
+++ js/themeswitcher.js
@@ -1,18 +1,18 @@
 function switchTheme() {
-    let current = document.getElementById('theme');
-    let swtchrbtn = document.getElementById('customSwitch1');
-    let ftr = document.getElementById('ftrr');
+	let current = document.getElementById('theme');
+	let swtchrbtn = document.getElementById('themetoggle');
+	let toggleIcon = document.getElementById('themetoggle-icon');
+	let appIcon = document.getElementById('app-icon');
 
-    if (current.getAttribute('href') == 'css/themes/[cosmo]bootstrap.min.css') {
-
-        current.href = 'css/themes/[cyborg]bootstrap.min.css';
-        swtchrbtn.value = "Light Theme";
-        ftr.style.backgroundColor = "--primary";
-
-    }
-    else {
-        current.href = 'css/themes/[cosmo]bootstrap.min.css';
-        swtchrbtn.value = "Dark Theme";
-        ftr.style.backgroundColor = "--primary";
-    }
+	if (current.getAttribute('href') == 'css/themes/[cosmo]bootstrap.min.css') {
+		current.href = 'css/themes/[cyborg]bootstrap.min.css';
+		swtchrbtn.style.backgroundColor = 'white';
+		toggleIcon.style.color = '#1b1b1b';
+		appIcon.src = 'icons/app_icon_light.png';
+	} else {
+		current.href = 'css/themes/[cosmo]bootstrap.min.css';
+		swtchrbtn.style.backgroundColor = '#1b1b1b';
+		toggleIcon.style.color = 'white';
+		appIcon.src = 'icons/app_icon_dark.png';
+	}
 }
